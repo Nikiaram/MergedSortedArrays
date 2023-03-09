85d39e2b606b31395aca3a2cbbe756787c09e9bc
Aram Nikolayan <anikolayan@authoritysoftware.com>
2023-03-09 22:50:34 +0400

    Add project files.

---
+++ MergedSortedArrays/MergingSortedArrays.cs
@@ -0,0 +1,43 @@
+﻿
+namespace MergedSortedArrays
+{
+    internal class MergingSortedArrays
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[] { 1, 2, 3, 40, 55, 66, 77, 88 };
+            int[] secondArray = new int[] { 9, 10, 11, 12, 13, 14, 15, 16 };
+
+            var thirdSortedArray = MergingTwoSortedArrays(firstArray, secondArray);
+
+            Test tests = new Test();
+            tests.FinalTesting(firstArray, secondArray, thirdSortedArray);
+        }
+
+        /// <summary>
+        /// Method returns merged collection of two sorted arrays
+        /// </summary>
+        /// <param name="firstSortedArray">firstSortedArray</param>
+        /// <param name="secondSortedArray">secondSortedArray</param>
+        /// <returns></returns>
+        static int[] MergingTwoSortedArrays(int[] firstSortedArray, int[] secondSortedArray)
+        {
+            var thirdArray = new int[firstSortedArray.Length + secondSortedArray.Length];
+            int x = 0, y = 0;
+            for (int i = 0; i < thirdArray.Length; i++)
+            {
+                if (y == secondSortedArray.Length || ((x < firstSortedArray.Length) && (firstSortedArray[x] < secondSortedArray[y])))
+                {
+                    thirdArray[i] = firstSortedArray[x];
+                    x++;
+                }
+                else
+                {
+                    thirdArray[i] = secondSortedArray[y];
+                    y++;
+                }
+            }
+            return thirdArray;
+        }
+    }
+}
---
+++ SortedTest/Test.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace SortedTest
+{
+    public class Test
+    {
+        [Fact]
+        public void FinalTesting(int[] firstSortedArray, int[] secondSortedArray, int[] thirdSortedArray)
+        {
+            var result = thirdSortedArray;
+            List<int> expectedResult = new List<int>(firstSortedArray.Length + secondSortedArray.Length);
+            expectedResult.AddRange(firstSortedArray);
+            expectedResult.AddRange(secondSortedArray);
+            expectedResult.Sort();
+
+            Assert.True(CheckResult(result, expectedResult));
+        }
+
+        private static bool CheckResult(int[] result, List<int> expectedResult)
+        {
+            if (result == null)
+            {
+                return false;
+            }
+
+            if (result.Length != expectedResult.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] != expectedResult[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
